ee6796d8d6e62a8959ab0774175684b4f20cd4c8
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-06-01 18:34:13 +0200

    Create HelloWorldApp.java

---
+++ hello_world_java/HelloWorldApp.java
@@ -0,0 +1,12 @@
+/* HelloWorldApp.java - Simple Hello Wolrd in Java coded without Eclipse & stuff - by StephaneAG - 2013 */
+
+/**
+*	The HelloWolrdApp class implements an App
+*	that simply prints 'Hello Wolrd !' to stdout
+*/
+
+class HelloWolrdApp {
+	public static void main(String[] args){
+		System.out.println("Hello World !"); // prints "Hello Wolrd !" to stdout
+	}
+}
